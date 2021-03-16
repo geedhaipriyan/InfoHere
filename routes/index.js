8a008f46c7dc10cba684ec49bf0c966aa4c81095
geedhaipriyan <geedhaipriyan1998@gmail.com>
2021-03-16 18:18:33 +0530

    controller

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ const HomeController = require('../controllers/home_controller');
 console.log('router loaded');
 
 router.get('/', HomeController.home);
+router.use('/user', require('./user'));
 
 module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+
+const UserController = require('../controllers/user_controller');
+
+
+router.get('/profile', UserController.profile);
+
+
+
+
+
+module.exports = router;
